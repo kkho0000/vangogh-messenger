9e3109b8d62671d7a3973bbf110f8ac1fe90e07e
Jenny12138962 <66908382+Jenny12138962@users.noreply.github.com>
2025-04-08 21:30:47 +0800

    Revert "Jump Ground"
    This reverts commit d14dcbaf3ba02fccd7c5c549af7a77e898fa7ff3.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,6 @@ public class PlayerController : MonoBehaviour
     private GameObject paint; // 线段数组
     Rigidbody rb;
     Animator animator; // 动画控制器
-    private bool isGrounded; // 判断是否在地面上
-
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +21,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        CheckGrounded(); // 检查是否在地面上
-
         float hor = Input.GetAxis("Horizontal");
         float ver = Input.GetAxis("Vertical");
 
@@ -35,26 +31,9 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector3(hor * moveSpeed, 0, rb.velocity.y); // 控制左右和前后移动
             transform.localScale = new Vector3(0.3f, 0.3f, hor > 0 ? 0.3f : -0.3f); // 控制角色朝向
         }
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) // 检测跳跃且在地面上
+        if (Input.GetKeyDown(KeyCode.Space)) // 检测跳跃
         {
             rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse); // 添加跳跃力
-            isGrounded = false; // 跳跃后不在地面上
-        }
-    }
-
-    // 检查是否在地面上
-    void CheckGrounded()
-    {
-        float rayLength = 0.1f; // 射线长度
-        RaycastHit hit;
-        // 从玩家位置向下发射射线
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, rayLength))
-        {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
         }
     }
 }
