cb3603e9b076fe7b51f7b9b4264c65c40b6ba30d
Jenny12138962 <66908382+Jenny12138962@users.noreply.github.com>
2025-04-09 20:22:17 +0800

    Merge

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private float timer = 0f;
+    private const float moveDuration = 3f;
+    private bool movingNegativeZ = true;
+    private int collisionCount = 0; // 记录碰撞次数
+    private float timer1 = 0f;
+    public GameObject playerObject;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        Vector3 direction = movingNegativeZ ? Vector3.forward : Vector3.back;
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+        if (timer >= moveDuration)
+        {
+            movingNegativeZ = !movingNegativeZ;
+            timer = 0f;
+        }
+
+        PlayerCamera player = playerObject.GetComponent<PlayerCamera>();
+        if (!player.isBlackAndWhite)
+        {
+            timer1 += Time.deltaTime;
+            if (timer1 >= 3f)
+            {
+                player.isBlackAndWhite = true;
+                player.ToggleRender();
+                timer1 = 0f;
+                collisionCount = 0;
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerCamera player = other.GetComponent<PlayerCamera>();
+            if (player != null)
+            {
+                collisionCount++;
+                if (collisionCount == 1)
+                {
+                    player.isBlackAndWhite = false;
+                    player.ToggleRender();
+                }
+                else if (collisionCount == 2)
+                {
+                    player.isBlackAndWhite = true;
+                    player.ToggleRender();
+                    player.Die();
+                    //player.ReturnToSpawnPoint();
+                    collisionCount = 0; // 重置碰撞次数
+                }
+            }
+        }
+    }
+}
